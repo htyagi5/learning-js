88bd4efdb02cb8c0899e3fc5e6d236d19ae1b68d
htyagi5 <harshit2310079@akgec.ac.in>
2024-11-26 10:06:56 +0530

    Update 04_comparision.js

--- js-hindi-youtube-main/01_basics/04_comparision.js
+++ js-hindi-youtube-main/01_basics/04_comparision.js
@@ -5,17 +5,16 @@
 // console.log(2 != 1);
 
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+// console.log("2" > 1);//true because it automatically convert itinto comparable  quantities string convert it into number
+// console.log("02" > 1);//trus
+//console.log("a2" > 1);//false
+console.log(null > 0);//false
+console.log(null == 0);//false
+console.log(null >= 0);//true
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
-
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
-
+console.log(undefined == 0);//false
+console.log(undefined > 0);//false
+console.log(undefined < 0);//false
 // === 
 
-console.log("2" === 2);
+console.log("2" === 2);//false
